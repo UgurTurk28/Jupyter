ee2250c75a47d8e95808ae2167af27cb0b0c64c7
UgurTurk28 <159319489+UgurTurk28@users.noreply.github.com>
2024-02-07 21:19:43 +0300

    Add files via upload

---
+++ _DataScienceEcosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem summerized.
+
+# **Objectives:**
+#    - Some of the languages that Data Scientists use
+#    - Commonly used libraries used by Data Science
+#    - Some mathematical functions
+#   
+
+# Some of the languages that Data Scientists use are:
+# 1. Python
+# 2. R 
+# 3. Java
+
+# Some of commonly used libraries used by Data Scientists include:
+# 1. Numpy
+# 2. Matplotlib
+# 3. Tenserflow
+
+# |Data Science Tools|
+# |:----------------:|
+# |Hadoop            |
+# |Spark             |
+# |Tenserflow        |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# This a simple arithmetic expression to multiply then add integers.
+
+# In[4]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+
+# In[4]:
+
+
+minute = 200
+hour = minute/60
+print(hour)
+
+
+# **Author** 
+# Uğur Türk
+# 
+
+# In[ ]:
+
+
+
+
